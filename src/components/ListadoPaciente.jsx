56ce77841029657e78038b99c25785c1b6a6f826
jefferson.martinez <jefferson.martinez@avaldigitallabs.com>
2022-01-31 22:29:07 -0500

    quito espacio

--- src/components/ListadoPaciente.jsx
+++ src/components/ListadoPaciente.jsx
@@ -3,7 +3,7 @@
  const ListadoPaciente = () => {
    return (
      <div className=' w-1/2 lg:w-3/5'>
-       <h2>listado pacientes hoy</h2>
+       <h2 className=''font-black text-3xl>listado pacientes hoy</h2>
        
      </div>
    )
